198998458cdbe7fe6a560b43d91ee78192604882
BorisRendon <boris.rendon@ufm.edu>
2020-07-06 20:03:11 -0600

    app connected to firebase

---
+++ CovidRelief/lib/screens/wrapper.dart
@@ -0,0 +1,16 @@
+import 'package:CovidRelief/screens/authenticate/authenticate.dart';
+import 'package:flutter/material.dart';
+
+import 'package:CovidRelief/screens/home/home.dart';
+
+
+
+
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+    
+  }
+}
--- CovidRelief/lib/services/auth.dart
+++ CovidRelief/lib/services/auth.dart
@@ -1,1 +1,27 @@
-//import 'fire'
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+
+  //sign anon
+  Future signInAnon() async {
+    try{
+      AuthResult result =  await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+
+    } catch(e){
+      print(e.toString());
+      return null;
+
+    }
+  }
+
+
+  //metodo para login con email y password
+  //metodo para registrarse
+  //cerrar sesion
+}
