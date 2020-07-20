d04464b717caa5735a134571d5b8c7cfb7468085
Maite de la Roca <delaroca.estrada21@gmail.com>
2020-07-20 16:51:55 -0600

    more fields added

--- CovidRelief/lib/screens/home/settings_form.dart
+++ CovidRelief/lib/screens/home/settings_form.dart
@@ -14,17 +14,17 @@ class _SettingsFormState extends State<SettingsForm> {
 
   final _formKey = GlobalKey<FormState>();
   final List<String> type = [ 'Busca ayuda' , 'Desea ayudar' ,'Ambos'];// esta es la lista de que va a poder elegir el usuario en forma de dropdown
-  final List<String> genero = [ 'Masculino' , 'Femenino'];
+  final List<String> gender = [ 'Masculino' , 'Femenino'];
 
   // valores
-  String _currentName; //ya
-  DateTime _currentBirthday; //ya
+  String _currentName; // text done
+  DateTime _currentBirthday; // calendar done
   String _currentCountry; // dropbox
   String _currentCreation; // not displaying in the app
-  String _currentgender; // dropbox
-  String _currentPhone;
-  String _currentState; // dropbox ya
-  String _currenType; ///ya
+  String _currentgender; // dropbox ya
+  String _currentPhone; // text
+  String _currentState; // not displaying in the app
+  String _currenType; // dropbox ya
   
   @override
   Widget build(BuildContext context) {
@@ -80,13 +80,38 @@ class _SettingsFormState extends State<SettingsForm> {
 
           // dropdown for country
           TextFormField(
-            decoration: textInputDecoration.copyWith(hintText: 'country'),
-            validator: (valcountry) => valcountry.isEmpty ? 'Please enter your country' : null,
+            decoration: textInputDecoration.copyWith(hintText: 'País'),
+            validator: (valcountry) => valcountry.isEmpty ? 'Por favor ingresa tu país' : null,
             onChanged: (valcountry) => setState(() => _currentCountry = valcountry),
           ),
 
           SizedBox(height: 20.0),
-          
+
+          // text for phone number
+          TextFormField(
+            decoration: textInputDecoration.copyWith(hintText: 'numero de teléfono'),
+            validator: (valphone) => valphone.isEmpty ? 'Por favor ingresa tu número de teléfono' : null,
+            onChanged: (valphone) => setState(() => _currentPhone = valphone),
+          ),
+
+          SizedBox(height: 20.0),
+
+          // dropdown for gender
+          // Lacks validation and display title
+          DropdownButtonFormField(
+            decoration: textInputDecoration,
+            // value: _currenType ??  '0',
+            items: gender.map((genders){
+              return DropdownMenuItem(
+                value: genders,
+                child: Text('$genders '),
+              );
+            }).toList(),
+            onChanged: (valgenders) => setState(() => _currentgender = valgenders),
+          ),
+
+          SizedBox(height: 20.0),
+
           // dropdown for type of profile
           // Lacks validation and display title
           DropdownButtonFormField(
@@ -100,6 +125,8 @@ class _SettingsFormState extends State<SettingsForm> {
             }).toList(),
             onChanged: (valtypes) => setState(() => _currenType = valtypes),
           ),
+
+          SizedBox(height: 20.0),
 
           // Move this button to other file
           /*
