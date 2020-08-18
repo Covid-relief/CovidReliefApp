17fe5dc7816f6e1599b20da6edf7385edfbc7f1d
andresryes <andresreyes@Andres-Mac.local>
2020-08-17 23:34:13 -0600

    ios app working with firebase

--- CovidRelief/ios/Runner/AppDelegate.swift
+++ CovidRelief/ios/Runner/AppDelegate.swift
@@ -1,10 +1,8 @@
 import UIKit
 import Flutter
-import Firebase
-
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, UIResponder, UIApplicationDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -12,12 +10,4 @@ import Firebase
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-  var window: UIWindow?
-
-    func application(_ application: UIApplication,
-      didFinishLaunchingWithOptions launchOptions:
-        [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-      FirebaseApp.configure()
-      return true
-    }
 }
